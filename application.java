db84013666ab532eff09836ae68328efda1803e1
sravanthiadapa <sravanthigangula.1@gmail.com>
2024-03-20 20:10:37 +0530

    modified application.java

--- application.java
+++ application.java
@@ -21,7 +21,31 @@ public class LocalVariable
       //Call method to display sum
       sum(number1, number2);
    }
+# new code added by developer feature 1.1
+public class MethodDemo
+{
+   public static void main(String[] args)
+   {
+      System.out.println("Advantage of methods.");
+      displayLine();
+      System.out.println("Write once use many times");
+      displayLine();
+   }
 
+   /**
+    *      The displayLine method displays a line.
+    */
+   public static void displayLine()
+   {
+      for (int i = 1; i <= 40; i++)
+      {
+         System.out.print("_");
+      }
+
+      System.out.println(" ");
+   }
+}
+#ended the code feature1.1  
    /**
     *  The sum method accept two arguments.
     */
